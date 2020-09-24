3ea44533de3ee185c6c0b42e8ba1812de7603603
isaac1110 <isaacwongchun@gmail.com>
2020-09-24 20:01:17 +0800

    Create temperature.pas

---
+++ temperature.pas
@@ -0,0 +1,13 @@
+var c,f,s:real;
+a:char;
+begin
+    readln(a);
+    if a='F' then begin
+        readln(f);
+        writeln((f-32)*5/9:10:2);
+    end
+    else begin
+        readln(c);
+        writeln(f*9/5+32:10:2);
+    end;
+end.
